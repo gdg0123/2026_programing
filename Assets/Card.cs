bbc0ac41c44ca7e06d022fa5454e6adf2b78cb2c
gdg0123 <gdg012377@gmail.com>
2026-04-21 16:22:17 +0900

    카드게임

---
+++ Assets/Card.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public int cardNumber;
+    public float rotationSpeed;
+    public bool isFront = true;
+    public bool isMatched = false;
+    private Quaternion flipRotation = Quaternion.Euler(0, 180f, 0);
+    private Quaternion originRotation = Quaternion.Euler(0, 0, 0);
+    public CardGame cardGame;
+
+    //����
+
+    //�Լ�
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //AND &&     OR ||
+
+        if(isFront)
+        {
+           transform.rotation = Quaternion.Slerp(transform.rotation, originRotation, rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+           transform.rotation = Quaternion.Slerp(transform.rotation, flipRotation, rotationSpeed * Time.deltaTime);
+        }
+
+        
+
+    }
+    
+
+
+    public void ClickCard()
+    {
+        if(!isMatched)
+        {
+            cardGame.OnClickCard(this);
+            //isFront = !isFront;
+        }
+        
+    }
+
+    //카드 돌리기
+
+    public void Flip(bool isFront)
+    {
+        this.isFront = isFront;
+    }
+
+
+    public void SetCardNumber(int newNumber)
+    {
+        text = GetComponentInChildren<TextMeshProUGUI>();
+
+        cardNumber = newNumber;
+
+        text.text = cardNumber.ToString();
+    }
+
+    public void ChangeColor(Color newColor)
+    {
+        GetComponent<Image>().color = newColor;
+    }
+
+    public void SetImage(Sprite sprite)
+    {
+        GetComponent<Image>().sprite = sprite;
+    }
+
+
+    
+}
---
+++ Assets/CardGame.cs
@@ -0,0 +1,133 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class CardGame : MonoBehaviour
+{
+    public List<Card> cards;
+    public List<Sprite> sprites;
+    private Card firstCard = null;
+    private Card secondCard = null;
+    private bool isChecking = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartGame();
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    List<int> GeneratePairNumbers(int cardCount)
+    {
+        //페어카드넘버생성
+        int pairCount = cardCount / 2;
+        List<int> newCardNumbers = new List<int>();
+
+        for(int i =  0; i < pairCount; ++i)
+        {
+            newCardNumbers.Add(i);
+            newCardNumbers.Add(i);
+        }
+
+        //셔플
+        for(int i = newCardNumbers.Count - 1; i > 0; i--)
+        {
+            int rnd = Random.Range(0, i + 1);
+            int temp = newCardNumbers[i];
+            newCardNumbers[i] = newCardNumbers[rnd];
+            newCardNumbers[rnd] = temp;
+        }
+
+        return newCardNumbers;
+    }
+
+
+    //게임루프
+    private void StartGame()
+    {
+        List<int> randomPairNumbers = GeneratePairNumbers(cards.Count);
+
+        for(int i = 0; i < cards.Count; ++i)
+        {
+            cards[i].SetCardNumber(randomPairNumbers[i]);
+            cards[i].SetImage(sprites[randomPairNumbers[i]]);
+            cards[i].isFront = false;
+        }
+    }
+
+    private void CheckCard()
+    {//두 카드가 맞는지
+
+        isChecking = true;
+
+        if(firstCard.cardNumber == secondCard.cardNumber)
+        {
+            firstCard.isMatched = true;
+            secondCard.isMatched = true;
+
+            firstCard.ChangeColor(Color.red);
+            secondCard.ChangeColor(Color.red);
+
+            firstCard = null;
+            secondCard = null;
+
+            isChecking = false;
+        }
+        else
+        { //다시원래대로
+          //지연
+            Invoke("HideCard", 0.6f);
+        }
+
+    }
+
+    private void HideCard()
+    {
+            firstCard.isFront = false;
+            secondCard.isFront = false;
+
+            firstCard.Flip(false);
+            secondCard.Flip(false);
+
+            firstCard = null;
+            secondCard = null;
+
+            isChecking = false;
+    }
+
+
+     //카드가선택되면호출
+    public void OnClickCard(Card card)
+    {
+        if(isChecking)
+        {
+           return;
+        }
+
+        if(firstCard == null)
+        {
+           firstCard = card;
+           firstCard.Flip(true);
+        }
+        else if(firstCard != card)
+        {
+           secondCard = card;
+           secondCard.Flip(true);
+        } 
+
+        if(firstCard != null && secondCard != null)
+        {
+            CheckCard();
+        }
+    }
+ 
+
+    
+}
